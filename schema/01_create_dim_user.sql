25881996091c11d284dd2a2e9cf49d563c0a5fdd
QAZYD <simondioresambata@gmail.com>
2025-10-06 14:55:17 +0800

    added dim_users

---
+++ schema/01_create_dim_user.sql
@@ -0,0 +1,17 @@
+CREATE TABLE dimUsers (
+    user_key INT AUTO_INCREMENT PRIMARY KEY,
+    source_user_id INT,
+    username VARCHAR(100),
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    address_1 VARCHAR(255),
+    address_2 VARCHAR(255),
+    city VARCHAR(100),
+    country VARCHAR(100),
+    zip_code VARCHAR(20),
+    phone_number VARCHAR(30),
+    date_of_birth DATE,
+    gender VARCHAR(10),
+    created_at DATETIME,
+    updated_at DATETIME
+);
